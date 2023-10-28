7e6cba720f9b2995d00ed41482e2d71f36413183
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2023-10-29 10:40:48 +1300

    Add google API Key.

--- src/pages/api/query.ts
+++ src/pages/api/query.ts
@@ -1,4 +1,4 @@
-import type {NextApiRequest, NextApiResponse} from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
@@ -8,8 +8,24 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
         // TODO：Invoke Google Custom Search Json API
         // see: https://developers.google.com/custom-search/v1/introduction
+        const query = req.query.q;
+
+        if (!query) {
+            return res.status(400).json({ message: 'Query parameter "q" is required' });
+        }
+
+        const url = `https://www.googleapis.com/customsearch/v1?q=${query}&key=${googleApiKey}&cx=${googleCx}`;
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            res.status(200).json(data);
+        } catch (error) {
+            console.error('Error fetching Google Custom Search API:', error);
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
 
     } else {
-        res.status(405).json({message: 'Method not allowed'});
+        res.status(405).json({ message: 'Method not allowed' });
     }
 }

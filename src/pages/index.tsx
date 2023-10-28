25a5311897b96975197469beebfdc02126544476
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2023-10-29 11:42:56 +1300

    Complete Search page style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,37 @@ import styles from "./index.module.css"
 
 
 export default function Home() {
+    const NavBar: React.FC = () => {
+        return (
+            <div className={styles.navbar}>
+                <span className={styles.navItem}>About</span>
+                <span className={styles.navItem}>Store</span>
+                <div className={styles.spacer}></div>
+                <span className={styles.navItem}>Gmail</span>
+                <span className={styles.navItem}>Images</span>
+                <div className={styles.icon}></div>
+                <div className={styles.icon}></div>
+            </div>
+        );
+    };
 
     return (
-        <div>
-            {
-                // TODO: HOME PAGE
-            }
+        // <div>
+        //     {
+        //         // TODO: HOME PAGE
+        //     }
 
-            Hello, World!
+        //     Hello, World!
+        // </div>
+
+        <div className={styles.container}>
+            <NavBar />
+            <img className={styles.googleLogo} src="/images/googlelogo_color.png" alt="Google Logo" />
+            <input className={styles.searchInput} type="text" placeholder="Search..." />
+            <div className={styles.buttons}>
+                <button className={styles.button}>Google Search</button>
+                <button className={styles.button}>I'm Feeling Lucky</button>
+            </div>
         </div>
     )
 }
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,16 +1,60 @@
-import React from "react";
-import {useRouter} from "next/router";
+import React, { useEffect, useState } from "react";
+
+import { useRouter } from "next/router";
 
 
 const SearchPage: React.FunctionComponent = () => {
     const router = useRouter();
-    const {q} = router.query;
+    const { q } = router.query;
+
+    const [results, setResults] = useState<any[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (!q) return;
+            setLoading(true);
+            setError(null);
+            try {
+                const response = await fetch(`/api/search?q=${q}`);
+                if (!response.ok) {
+                    throw new Error(`Error: ${response.status}`);
+                }
+                const data = await response.json();
+                setResults(data.items);
+            } catch (error) {
+                console.error(error);
+                setError("Failed to fetch results");
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, [q]);
 
     return (
         <div>
-            {
-                // TODO: Search Page
-            }
+            {/* {
+                // TODO: Search Page how to do
+            } */}
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error}</p>}
+            {results.length > 0 ? (
+                <ul>
+                    {results.map((result, index) => (
+                        <li key={index}>
+                            <h3>{result.title}</h3>
+                            <p>{result.snippet}</p>
+                            <a href={result.link} target="_blank" rel="noopener noreferrer">
+                                {result.displayLink}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No results found</p>
+            )}
             Search Page: {q}
         </div>
     )
